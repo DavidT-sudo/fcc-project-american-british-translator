0f31ede633e3ef4fcf5d97507678df67aa205187
davidt@hp <davidt@hp.com>
2024-02-16 16:28:08 +0200

    feat: add tranlator.js in the components

--- components/translator.js
+++ components/translator.js
@@ -3,8 +3,116 @@ const americanToBritishSpelling = require('./american-to-british-spelling.js');
 const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
-class Translator {
+function reverseObj(obj) {
+    let reversedObj = {};
 
+    Object.entries(obj).map(([key, value]) => {
+        reversedObj[value] = key;
+    });
+
+    return reversedObj;
+}
+
+class Translator {
+    toBritish(inputText) {
+        //set up all words to be translated using object lookup
+        const translateObj = {
+            ...americanOnly,
+            ...americanToBritishSpelling
+        };
+
+        const titles = americanToBritishTitles;
+
+        let timeRegex = /([1-9]|1[012]):[0-5][0-9]/g;
+
+        let translatedHtml = this.translate(inputText, translateObj, titles, timeRegex, "toBritish");
+
+        if(translatedHtml == null) {
+            return inputText;
+        }
+
+        return translatedHtml;
+
+    }
+
+    toAmerican(inputText) {
+        const translateObj = {
+            ...britishOnly,
+            ...reverseObj(americanToBritishSpelling)
+        };
+
+        const titles = reverseObj(americanToBritishTitles);
+
+        const timeRegex = /([1-9]|1[012])\.[0-5][0-9]/g;
+
+        let translatedHtml = this.translate(inputText, translateObj, titles, timeRegex, "toAmerican");
+
+        if(translatedHtml == null) {
+            return inputText;
+        }
+
+        return translatedHtml;
+
+    }
+
+    translate(inputText, translateObj, titles, timeRegex, locale) {
+        
+        let lowerCaseText = inputText.toLowerCase();
+        let matches = {};
+
+        Object.entries(titles).map(([key, value]) => {
+            if (lowerCaseText.includes(key)) {
+                console.log("Hit on word ..... ", key);
+                matches[key] = value.charAt(0).toUpperCase() + value.slice(1,);
+            }
+        });
+
+        Object.entries(translateObj).map(([key, value]) => {
+            if(lowerCaseText.includes(key)) {
+                console.log("Hit on word ..... ", key);
+                matches[key] = value;
+            }
+        });
+
+        let timeMatches = lowerCaseText.match(timeRegex);
+
+        console.log("timeMatches .... ", timeMatches);
+
+        if(timeMatches) {
+
+            if(locale == "toBritish") {
+                console.log("to british time");
+                timeMatches.map((elem) => {
+                    matches[elem] = elem.replace(":", ".");
+                });
+
+            } else {
+                console.log("to american time...");
+                timeMatches.map( (elem) => {
+                    matches[elem] = elem.replace(".", ":");
+                })
+            }
+
+            console.log(matches);
+        }
+
+
+        let regex = new RegExp(Object.keys(matches).join("|"), "ig");
+
+        let translatedText = inputText.replace(regex, (matchedValues) => {
+            return matches[matchedValues.toLowerCase()];
+        });
+
+        let highlightedText = inputText.replace(regex, (matchedValues) => {
+            return `<span class="highlight">${matches[matchedValues.toLowerCase()]}</span>`;
+        });
+
+        if(Object.keys(matches).length == 0) {
+            return null;
+        }
+
+        return [translatedText, highlightedText];
+    }
 }
 
 module.exports = Translator;
